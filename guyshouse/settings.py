591bddbc84c4777ed58c1b90bdc800bdbc7f1075
kominhyuk <komhgogo@gmail.com>
2021-12-13 21:17:25 +0900

    모델링 리뷰반영

--- guyshouse/settings.py
+++ guyshouse/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'core',
+    'users',
+    'programs',
 ]
 
 MIDDLEWARE = [
---
+++ users/models.py
@@ -0,0 +1,35 @@
+from django.db       import models
+
+from core.models     import TimeStampModel
+from programs.models import Program
+
+class User(TimeStampModel):
+    nickname          = models.CharField(max_length=100)
+    email             = models.CharField(max_length=300, unique=True)
+    profile_image_url = models.CharField(max_length=2000)
+    is_host           = models.BooleanField(default=False)
+    host_description  = models.CharField(max_length=300, null=True)
+
+    class Meta:
+        db_table = 'users'
+
+class Like(models.Model):
+    user    = models.ForeignKey('User', on_delete=models.CASCADE)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'likes'
+        constraints = [
+            models.UniqueConstraint(fields=['user_id', 'program_id'], name="unique likes")
+        ]
+
+class Review(TimeStampModel):
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    user    = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    context = models.CharField(max_length=300)
+
+    class Meta:
+        db_table = 'reviews'
+        constraints = [
+            models.UniqueConstraint(fields=['user_id', 'program_id'], name="unique reviews")
+        ]
