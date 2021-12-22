d87e85da675985e0a5fea40f012039ac9e7d3311
kominhyuk <komhgogo@gmail.com>
2021-12-22 10:30:24 +0900

    CREATED : DetailView, ReserveView

--- programs/urls.py
+++ programs/urls.py
@@ -1,8 +1,10 @@
 from django.urls    import path
-
-from programs.views import ProgramListView
+from programs.views import ProgramDetailView, ProgramView, ReserveView,ProgramListView
 
 
 urlpatterns = [
-    path('', ProgramListView.as_view())
+    path('', ProgramListView.as_view()),
+    path('/hosting', ProgramView.as_view()),
+    path('/detail/<int:program_id>', ProgramDetailView.as_view()),
+    path('/detail/<int:program_id>/reserve', ReserveView.as_view()),
 ]
--- programs/views.py
+++ programs/views.py
@@ -7,7 +7,7 @@ from django.db              import transaction
 
 from django.db.models       import Q
 
-from programs.models        import DetailImage, Program, Category, ProgramCategory, ProgramQuestion, ScreeningAnswer, ScreeningQuestion
+from programs.models        import DetailImage, Program, Category, ProgramCategory, ProgramQuestion, ScreeningAnswer, ScreeningQuestion, QuestionAnswer
 from users.models           import User
 from programs.file_handler  import FileHanlder
 from guyshouse.settings     import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_STORAGE_BUCKET, AWS_REGION
@@ -19,7 +19,8 @@ boto3_client = boto3.client(
 )
 
 class ProgramView(View):
-    @host_check_decorator
+    @signin_decorator
+    @host_decorator
     def post(self, request):
         try:
             file_hanlder    = FileHanlder(boto3_client, AWS_STORAGE_BUCKET, AWS_REGION)
@@ -52,22 +53,33 @@ class ProgramView(View):
 
         except KeyError:
             return JsonResponse({"message":"KEY_ERROR"},status=400)
+
+        except transaction.TransactionManagementError:
+            return JsonResponse({"message":"KEY_ERROR"},status=400)
     
-    @host_check_decorator
+    @signin_decorator
+    @host_decorator
     def get(self, request):
         user     = request.user
         offset   = request.GET.get('offset', 0)
         limit    = request.GET.get('limit', 100)
         programs = Program.objects.filter(user = user)[offset:offset+limit]
-        results = [
-            {
+        results = {
+            'programs' : [{
+                'program_id'     : program.id,
                 'name'           : program.name,
                 'description'    : program.description,
                 'address'        : program.address,
                 'thumbnail_image': program.thumbnail_image_url
+            }for program in programs],
+            'host' : {
+                'nickname' : user.nickname,
+                'email' : user.email,
+                'profile_image' : user.profile_image_url,
+                'host_description' : user.host_description
             }
-            for program in programs
-            ]
+        }
+
         return JsonResponse({'result' : results}, status = 200)
 
 class ProgramListView(View):
@@ -100,3 +112,82 @@ class ProgramListView(View):
         } for program in programs]
         
         return JsonResponse({'result' : results}, status = 200)
+
+class ProgramDetailView(View):
+    def get(self, request, program_id):
+        try:
+            program      = Program.objects.select_related('user').get(id=program_id)
+            detail_images = DetailImage.objects.filter(program = program)
+            result = {
+                'id'                 : program.id,
+                'name'               : program.name,
+                'description'        : program.description,
+                'price'              : program.price,
+                'address'            : program.address,
+                'start_date'         : program.start_date,
+                'running_time'       : program.running_time,
+                'supply'             : program.supply,
+                'limit'              : program.limit,
+                'thumbnail_image_url': program.thumbnail_image_url,
+                'detail_image_url'   : [{'id' : detail_image.id,'image' : detail_image.image_url} for detail_image in detail_images],
+                'user'               : {
+                    'id'               : program.user.id,
+                    'nickname'         : program.user.nickname,
+                    'profile_image_url': program.user.profile_image_url,
+                    'host_description ': program.user.host_description
+                }
+            }
+            return JsonResponse({'result' : result}, status = 200)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
+        except Program.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID_PROGRAM'}, status = 404)
+
+class ReserveView(View):
+    @signin_decorator
+    def get(self, request, program_id):
+        program   = Program.objects.get(id = program_id)
+        questions = ProgramQuestion.objects.select_related('question').filter(program = program)
+        results   = [
+            {
+                'question_id': question.question.id,
+                'context'    : question.question.question
+            }
+            for question in questions
+        ]
+
+        return JsonResponse({"result":results}, status = 200)
+
+    @signin_decorator
+    def post(self, request, program_id):
+        try :
+            data          = json.loads(request.body)
+            user          = request.user
+            program       = Program.objects.get(id = program_id)
+            question_list = ScreeningQuestion.objects.filter(program = program).order_by('id')
+
+            answers = [answer for answer in data.values()]
+            answer_list = []
+
+            for answer in answers:
+                answer = ScreeningAnswer.objects.create(answer = answer, user = user)
+                answer_list.append(answer)
+
+            QuestionAnswer.objects.bulk_create(
+                [
+                    QuestionAnswer(
+                        answer   = answer_list[question_number],
+                        question = question_list[question_number]
+                    ) for question_number in range(question_list.count()) 
+                ]
+            )
+
+            return JsonResponse({'message':'SUCCESS'}, status = 201)
+
+        except KeyError:
+            return JsonResponse({"message":"KEY_ERROR"}, status = 400)
+
+        except IndexError:
+            return JsonResponse({"message":"INDEX_ERROR"}, status =400)
